5254f0701787d51b6628b5fcc834e669637c063a
jossAcosta <josselineacostamtz@gmail.com>
2020-05-07 19:47:14 -0500

    Updates

---
+++ precache-manifest.d7a178d765fc762d1fefc6b43b13d4db.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58691dc6de90584316ba7d205b329125",
+    "url": "/moodvie/index.html"
+  },
+  {
+    "revision": "b20d38ae3c43fc5fad60",
+    "url": "/moodvie/static/css/main.601e5523.chunk.css"
+  },
+  {
+    "revision": "938498ce88cd2cd52918",
+    "url": "/moodvie/static/js/2.5688c461.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/moodvie/static/js/2.5688c461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b20d38ae3c43fc5fad60",
+    "url": "/moodvie/static/js/main.750f09a0.chunk.js"
+  },
+  {
+    "revision": "8023c638031854f576c5",
+    "url": "/moodvie/static/js/runtime-main.24c482d0.js"
+  },
+  {
+    "revision": "78acabf9c426b2cb93c3dacbe97e0d6a",
+    "url": "/moodvie/static/media/action.78acabf9.svg"
+  },
+  {
+    "revision": "e11f5b266dd70d32d874eaa37775168a",
+    "url": "/moodvie/static/media/back.e11f5b26.svg"
+  },
+  {
+    "revision": "0d228ebfdbe79583ea6a7c333ae65c84",
+    "url": "/moodvie/static/media/clockLess.0d228ebf.svg"
+  },
+  {
+    "revision": "c350e5e92555b9695cf471824ddb05c3",
+    "url": "/moodvie/static/media/clockMore.c350e5e9.svg"
+  },
+  {
+    "revision": "0406958b59d9c523d76e1c165ea4518b",
+    "url": "/moodvie/static/media/home.0406958b.svg"
+  },
+  {
+    "revision": "b5a6ffda773aa193a1b3ebed568d1bd3",
+    "url": "/moodvie/static/media/logo.b5a6ffda.svg"
+  },
+  {
+    "revision": "46f8ec1ff2e63b0a88f95fe0c2e0ed4f",
+    "url": "/moodvie/static/media/musical.46f8ec1f.svg"
+  },
+  {
+    "revision": "bf851c6f1f98fe20a1b16238bdc11ece",
+    "url": "/moodvie/static/media/romance.bf851c6f.svg"
+  },
+  {
+    "revision": "be1f7e526465c5882437ba13d35d216d",
+    "url": "/moodvie/static/media/thriller.be1f7e52.svg"
+  }
+]);
